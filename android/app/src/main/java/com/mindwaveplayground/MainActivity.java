071fce6888199832a4eafeaf0d65b106e915a34c
zhong666 <aa900031@gmail.com>
2017-12-05 12:01:12 +0800

    重新命名完成

--- android/app/src/main/java/com/mindwaveplayground/MainActivity.java
+++ android/app/src/main/java/com/mindwaveplayground/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativemindwaveproject;
+package com.mindwaveplayground;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ReactNativeMindWaveProject";
+        return "MindWavePlayground";
     }
 }
